cd4ebe4a7207e1d992872d2437eb210da0f1120d
Matheus Braun <matheeusbl@gmail.com>
2020-07-25 21:20:17 -0300

    refactor: changing url param id

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,6 @@ import UrlShortenerController from '../controllers/UrlShortenerController';
 const routes = Router();
 
 routes.post('/url-shortener', UrlShortenerController.create);
-routes.get('/url-shortener/:urlid', UrlShortenerController.show);
+routes.get('/url-shortener/:shortUrlId', UrlShortenerController.show);
 
 export default routes;
